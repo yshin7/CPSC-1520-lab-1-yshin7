4b0a657f04cbd5d4916e855ab835c8d7c7f42bec
yshin7 <snsla10@gmail.com>
2024-01-26 10:24:38 -0700

    Add contants, event listeners, functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
  
+const openModal = document.querySelector("#open-modal");
+const modalWindow = document.querySelector("#modal-window");
+const closeButton = document.querySelector("#close-button");
+
+openModal.addEventListener("click", onModal);
+closeButton.addEventListener("click", onClose);
+
+function onModal (e) {
+    modalWindow.showModal();
+}
+
+function onClose (e) {
+    modalWindow.close();
+}
+
